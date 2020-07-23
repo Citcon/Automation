28c52abd6bdd12d754c491dcb98437aa65de27c2
huang.xiaoqian <huang.xiaoqian@citcon.cn>
2020-07-23 19:33:20 +0800

    code

---
+++ conf/db.js
@@ -0,0 +1,12 @@
+
+
+const MYSQL_CONF = {
+    host: 'darth-uat.internal.citconpay.com',
+    user: 'qingyan',
+    password: 'Wqs$8$X7US',
+    port: '3306',
+    database: 'citcon_user'
+}
+
+
+module.exports = MYSQL_CONF //只有一个值，不能用{}？
---
+++ controller/code.js
@@ -0,0 +1,12 @@
+const { exec } = require('../db/mysql')
+
+
+const getCode = () =>{
+    const sql = `select auth_code from user_auth_codes where user_id= 11 
+    order by time_created desc limit 1`
+    return exec(sql).then(rows=>{
+        return rows[0].auth_code //return 出去的还是Promise
+    })
+}
+
+module.exports = getCode
